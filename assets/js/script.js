b35c05eeaf50c0941a247dc05f69462da767c885
loudagnese <louis.dagnese@gmail.com>
2022-06-01 20:33:01 -0700

    scaffold and psuedocode

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// declare the questions array
+
+// main function startGame when button is clicked
+    // First thing it does is start the timer
+    // pulls a question from the array and sends it to the questions handler
+    // needs to evaluate the timer and if timer = 0 then game over
+    // 
+
+// handler for showing the questions on the page
+    // Needs to get the question from the main function
+    // builds the html and put an event listener on the buttons
+
+// handler for displaying the score on the page
+    // updates when a question is answered
+
+//  handler for the timer
+    // starts the timer on the page
+    // use setTimeout recursively to display the timer
+
+//  handler to display the getHighScore when button clicked
+    // if highScore doesn't exist instantiate new array of objects
+    // sort the highScore Array from localStorage
+
+//  handler for storing the highScore to localStorage
+    // if highScore doesn't exist in localStorage instantiate a new array 
+    // (possible function to handle this outside the function)?
+
+// event listeners
+    // StartButton
+    // View HighScore Button
